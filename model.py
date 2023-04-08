39ebb143d39e08574ddf500de5be8a61b0cf8bb5
nikit <nikitka.lyashko@bk.ru>
2023-04-08 17:14:54 +0300

    flowers

--- model.py
+++ model.py
@@ -4,20 +4,24 @@ import pygame
 width_win=300
 height_win=300
 rect=pygame.Rect(250,0,50,100)
+flower_rect = pygame.Rect(100, 250, 50, 100)
+flower_rect.left=0
+
 
 def drive_rect():
-    global height_win, width_win
+    global height_win, width_win, flower_rect
     rect.bottom+=5
 
     if rect.top<height_win and rect.bottom>=height_win:
         pygame.display.set_mode([width_win+3,height_win])
         width_win+=3
 
+    flower_rect.width = width_win / 2
+
     if rect.top>=height_win:
         rect.bottom=0
         rect.width = random.randint(10, 50)
         rect.height = random.randint(10, 100)
         rect.left = random.randint(0, width_win)
-
 
 
--- view.py
+++ view.py
@@ -6,12 +6,18 @@ import model
 
 def background():
 
+
     window.fill([255,70,70])
     small_water=pygame.transform.scale(water,model.rect.size)
     window.blit(small_water,[model.rect.x,model.rect.y])
+
     height_flovers_new=flower_1.get_height()*50/flower_1.get_width()
     flower_1_start=pygame.transform.scale(flower_1,[50,height_flovers_new])
     window.blit(flower_1_start, [0, model.height_win-flower_1_start.get_height()])
+
+    rect_fl=pygame.draw.rect(window,[10,100,62],model.flower_rect)
+
+
 
     pygame.display.flip()
 
